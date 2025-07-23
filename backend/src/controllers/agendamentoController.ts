98545713f8fa15b2ae12dab15d84692b44cb659d
francisco328react <fseveriano56@hotmail.com>
2025-07-23 15:37:18 -0400

    feat: Criado rotas GET, PUT, DELETE de agendamentos

--- backend/src/controllers/agendamentoController.ts
+++ backend/src/controllers/agendamentoController.ts
@@ -41,3 +41,101 @@ export async function createAgendamento(req: Request, res: Response): Promise<vo
         return
     }
 }
+
+export async function getAllAgendamentos(req: Request, res: Response): Promise<void> {
+    try {
+        const agendamentos = await prisma.agendamento.findMany({
+            include: {
+                paciente: true,
+                exame: true,
+                medico: true,
+            },
+        });
+
+        res.json(agendamentos);
+        return
+    } catch (error) {
+        console.error("Erro ao buscar agendamentos", error);
+        res.status(500).json({ message: "Erro ao buscar agendamentos" });
+        return
+    }
+}
+
+export async function getAgendamentoById(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+
+    try {
+        const agendamento = await prisma.agendamento.findUnique({
+            where: { id },
+            include: {
+                paciente: true,
+                exame: true,
+                medico: true
+            },
+        });
+
+        if(!agendamento) {
+            res.status(400).json({ message: "Agendamento não encontrado" });
+            return
+        }
+
+        res.json(agendamento);
+        return
+    } catch (error) {
+        console.error("Erro ao buscar agendamento", error);
+        res.status(500).json({ message: "Erro ao buscar agendamento" });
+        return
+    }
+}
+
+export async function updateAgendamento(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+    const { dataHora, pacienteId, exameId, medicoId } = req.body;
+
+    try {
+        const existeAgendamento = await prisma.agendamento.findFirst({
+            where: {
+                dataHora: new Date(dataHora),
+                exameId,
+                NOT: { id },
+            },
+        });
+
+        if(existeAgendamento) {
+            res.status(400).json({ message: "Novo horário indisponível" });
+            return
+        }
+
+        const agendamento = await prisma.agendamento.update({
+            where: { id },
+            data: {
+                dataHora: new Date(dataHora),
+                pacienteId,
+                exameId,
+                medicoId,
+            },
+        });
+
+        res.json(agendamento);
+        return
+    } catch (error) {
+        console.error("Erro ao atualizar agendamento:", error);
+        res.status(500).json({ message: "Erro ao atualizar agendamento" });
+        return
+    }
+}
+
+export async function deleteAgendamento(req: Request, res: Response): Promise<void> {
+    const { id } = req.params;
+
+    try {
+        await prisma.agendamento.delete({ where: { id } });
+
+        res.json({ message: "Agendamento deletado com sucesso" });
+        return
+    } catch (error) {
+        console.error("Erro ao deletar agendamento", error);
+        res.status(500).json({ message: "Erro ao deletar agendamento" });
+        return
+    }
+}
--- backend/src/routes/agendamentoRoutes.ts
+++ backend/src/routes/agendamentoRoutes.ts
@@ -1,9 +1,13 @@
 import { Router } from "express";
 import { authenticate } from "../middlewares/authMiddleware";
-import { createAgendamento } from "../controllers/agendamentoController";
+import { createAgendamento, getAllAgendamentos, getAgendamentoById, updateAgendamento, deleteAgendamento } from "../controllers/agendamentoController";
 
 const router = Router();
 
 router.post("/agendamento", authenticate, createAgendamento);
+router.get("/agendamento", authenticate, getAllAgendamentos);
+router.get("/agendamento/:id", authenticate, getAgendamentoById);
+router.put("/agendamento/:id", authenticate, updateAgendamento);
+router.delete("/agendamento/:id", authenticate, deleteAgendamento);
 
 export default router;
